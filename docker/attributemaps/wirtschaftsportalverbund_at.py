dd75d79c6d95587ec7f33845719bd2710ac63eb0
war <thomas@warwaris.at>
2021-01-20 15:24:45 +0100

    [fix] wkis redirect attribute

---
+++ docker/attributemaps/wirtschaftsportalverbund_at.py
@@ -0,0 +1,21 @@
+MAP = {
+  'identifier': 'urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified',
+  'fro': {
+    'http://schemas.wko.at/ws/2014/02/identity/claims/redirect': 'wkis_redirect',
+    'urn:oid:1.3.88': 'gln',
+    'http://wirtschaftsportalverbund.at/ns/identity/claims/2016/04/authenticationClass': 'authenticationClass',
+    'http://wirtschaftsportalverbund.at/ns/identity/claims/2016/04/registrationClassOrg': 'registrationClassOrg',
+    'http://wirtschaftsportalverbund.at/ns/identity/claims/2016/04/registrationClassUser': 'registrationClassUser',
+    'http://schemas.wko.at/ws/2014/06/identity/claims/possiblerole': 'PossibleRoles',
+    '1.2.40.0.10.2.1.1.261.100': 'orgSourcePin',
+  },
+  'to': {
+    'wkis_redirect': 'http://schemas.wko.at/ws/2014/02/identity/claims/redirect',
+    'gln': 'urn:oid:1.3.88',
+    'authenticationClass': 'http://wirtschaftsportalverbund.at/ns/identity/claims/2016/04/authenticationClass',
+    'registrationClassOrg': 'http://wirtschaftsportalverbund.at/ns/identity/claims/2016/04/registrationClassOrg',
+    'registrationClassUser': 'http://wirtschaftsportalverbund.at/ns/identity/claims/2016/04/registrationClassUser',
+    'PossibleRoles': 'http://schemas.wko.at/ws/2014/06/identity/claims/possiblerole',
+    'orgSourcePin': '1.2.40.0.10.2.1.1.261.100',
+  }
+}
